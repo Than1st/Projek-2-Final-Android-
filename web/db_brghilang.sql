e2873cd1ac14d27486616f9e5e0a0aac8e250e93
Than1st <1911500641@student.budiluhur.ac.id>
2020-03-09 20:35:33 +0700

    Tambah SQL

--- web/db_brghilang.sql
+++ web/db_brghilang.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 06, 2020 at 02:28 PM
+-- Generation Time: Mar 09, 2020 at 02:34 PM
 -- Server version: 10.4.6-MariaDB
 -- PHP Version: 7.3.9
 
@@ -21,6 +21,32 @@ SET time_zone = "+00:00";
 --
 -- Database: `db_brghilang`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `brghilang`
+--
+
+CREATE TABLE `brghilang` (
+  `id_brghilang` varchar(3) NOT NULL,
+  `nama` varchar(30) NOT NULL,
+  `kategori` varchar(20) NOT NULL,
+  `detail` varchar(30) NOT NULL,
+  `waktu_ditemukan` datetime NOT NULL,
+  `waktu_dikembalikan` datetime NOT NULL,
+  `status` enum('0','1') NOT NULL,
+  `src` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `brghilang`
+--
+
+INSERT INTO `brghilang` (`id_brghilang`, `nama`, `kategori`, `detail`, `waktu_ditemukan`, `waktu_dikembalikan`, `status`, `src`) VALUES
+('1', 'Flashdisk HP', 'Electronic', 'stainless steel', '2020-03-07 11:32:37', '0000-00-00 00:00:00', '0', 'http://192.168.42.59/barang_hilang/img/1.jpg'),
+('2', 'Laptop Macbook', 'Electronic', 'Tipis, putih', '2020-03-07 11:34:02', '0000-00-00 00:00:00', '0', 'http://192.168.42.59/barang_hilang/img/2.jpg\r\n'),
+('3', 'Topi Hitam', 'Pakaian', 'warna Hitam', '2020-03-07 13:51:52', '2020-03-07 13:51:52', '0', 'http://192.168.42.59/barang_hilang/img/3.jpg');
 
 -- --------------------------------------------------------
 
@@ -46,6 +72,12 @@ INSERT INTO `user` (`nim`, `nama`, `jabatan`, `notelp`, `password`) VALUES
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `brghilang`
+--
+ALTER TABLE `brghilang`
+  ADD PRIMARY KEY (`id_brghilang`);
 
 --
 -- Indexes for table `user`
